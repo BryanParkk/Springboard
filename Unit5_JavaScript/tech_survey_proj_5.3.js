7297b88c4612a72bf476132b24b4e6392f86b947
Byeongil Park <bryan1010110@gmail.com>
2024-10-24 19:13:59 -0400

    10/24 study

---
+++ Unit5_JavaScript/tech_survey_proj_5.3.js
@@ -0,0 +1,27 @@
+// The Story of Caesar's Traditional Secret Party
+// Q2 Answer
+
+// let hostName = "Julius Caesar"; // String
+// let partyLocation = "Rome"; // String 
+// let partyDate = "05/27/1986"; // String
+// let dressCode = "Tuxedo"; // String
+// let bringGift = true; // Boolean
+// let cipherShift = 3; // Number
+// let encMsg = ""; // String
+// let decMsg = ""; // String
+
+
+/////////////////////////////////////////////////////////////
+// The Story of Caesar's Traditional Secret Party
+// Q2 Answer
+
+const hostName = "Julius Caesar"; // String
+let partyLocation = "Rome"; // String 
+const partyDate = "05/27/1986"; // String
+const dressCode = "Tuxedo"; // String
+let bringGift = true; // Boolean
+const cipherShift = 3; // Number
+let encMsg = ""; // This will change when we encode the message with the original party location.
+let decMsg = ""; // This will change when we decode the encrypted message to reveal the party location.
+
+Number.isInteger(cipherShift)
