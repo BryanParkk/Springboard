74aaaacaaac7b310b3553b06a4c272ada1211c91
Byeongil Park <bryan1010110@gmail.com>
2024-10-24 23:38:46 -0400

    10/24 study

--- Unit5_JavaScript/properties.js
+++ Unit5_JavaScript/properties.js
@@ -1,5 +1,5 @@
 
-let hello = "Hello, World!"
+let hello = "      Hello, World!      ";
 
 
 // thing.method()
@@ -8,3 +8,14 @@ console.log( Number.isInteger(hello) );
 console.log( typeof(hello) );
 console.log( hello.toUpperCase() );
 console.log( hello.toLowerCase() );
+console.log( hello.trim() );
+console.log( hello.trim().toUpperCase() );
+
+// thing.method(argument)
+console.log( hello.indexOf('Hello') ); // not exist is representatio -1
+console.log( "Hello World".slice(0,2) );
+console.log( hello.replace('World','JavaScript') );
+console.log(  );
+console.log(  );
+console.log(  );
+
