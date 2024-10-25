722785ddedac18c0cf5429e047c3d91e0e816616
Byeongil Park <bryan1010110@gmail.com>
2024-10-25 14:39:16 -0400

    update tech_survey_proj_5

---
+++ Unit5_JavaScript/tech_survey_proj_5.3-2.js
@@ -0,0 +1,32 @@
+
+const friend = "BRUTUS";
+const shiftValue = 3;
+
+
+//Step 1
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+//Step 2
+const firstLetter = friend[0];
+const step2 = alphabet.indexOf(firstLetter.toLowerCase());
+
+
+// Question 1
+// index starts 0, not 1. B is second letter of alphabet, so answer is going to be 1.
+
+//Step 3
+const step3 = alphabet[step2+shiftValue];
+
+// Question 2
+// Modulo(%) operator can warp around.
+
+//Step 4
+const alphabetLength = alphabet.length;
+
+//Step 5
+const step5 = (step2+shiftValue)%alphabetLength;
+
+//Step 6
+const encMsg = 'EUXWXV';
+const send = encMsg.slice(0, 3);
+console.log(send);
