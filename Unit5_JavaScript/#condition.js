6408627e0872e8fffcbefd741e11c47362185297
Byeongil Park <bryan1010110@gmail.com>
2024-10-26 15:19:29 -0400

    update

--- Unit5_JavaScript/#condition.js
+++ Unit5_JavaScript/#condition.js
@@ -7,6 +7,16 @@ if(true) {
     console.log("World, Hello");
 }
 
+//Comparisons
+> // greater than
+< // less than
+>= // greater than or equal to
+<= // less than or equal to
+== // equality
+!= // not equal
+=== // strict equality 
+!= // strict non-equality
+
 // Boolean
 Boolean('hi');             // true
 Boolean('');               // false
