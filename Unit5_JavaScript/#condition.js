95e8ae870f68d642b07ad1a55c778d9a021dcb3e
Byeongil Park <bryan1010110@gmail.com>
2024-10-26 14:47:37 -0400

    condition update

--- Unit5_JavaScript/#condition.js
+++ Unit5_JavaScript/#condition.js
@@ -1,7 +1,40 @@
 // Conditional Logic
 
+//if condition
 if(true) {
     console.log("Hello World");
 }else {
     console.log("World, Hello");
 }
+
+// Boolean
+Boolean('hi');             // true
+Boolean('');               // false
+Boolean(favoriteNumber);   // false
+Boolean(null);             // false
+Boolean(1);                // true
+Boolean(undefined);        // false
+Boolean('cat');            // true
+Boolean(-0);               // false
+Boolean(-1);               // true 
+
+//!! bangbang
+let favoriteNumber = 0;
+
+!!'hi';             // true
+!!'';               // false
+!!favoriteNumber;   // false
+!!null;             // false
+!!1;                // true
+!!undefined;        // false
+!!'cat';            // true
+
+//Ternary operator
+( expression to check ) ? statement if true : statement if false;
+let guess = 15; // guess a number between 1 to 20
+guess > answer ? console.log('too high') : console.log('too low');
+
+let guess = 15;
+let wasCorrect = (guess === 15) ? 'Your correct!' : 'Incorrect';
+console.log(wasCorrect) // => 'Your correct!'
+
