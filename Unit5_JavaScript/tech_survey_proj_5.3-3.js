f689a84efcfeff2c1649678c4ec36f07d9ac93ea
Byeongil Park <bryan1010110@gmail.com>
2024-10-26 09:22:58 -0400

    step 4

--- Unit5_JavaScript/tech_survey_proj_5.3-3.js
+++ Unit5_JavaScript/tech_survey_proj_5.3-3.js
@@ -5,6 +5,17 @@ const ranDecimal = Math.random();
 const min = 3;
 const max = 33;
 
-//Step 3
+//Question 1
 //I have to include max value
 
+//Step 3
+const ranValue = ranDecimal * 30 + 3 + 1;
+
+//Question 2
+//I didn't use range variable.
+
+//Step 4
+const adjValue = Math.floor(ranValue);
+
+
+console.log(adjValue);
