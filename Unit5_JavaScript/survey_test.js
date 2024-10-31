feba3e1a938ef2fa2d97af662e03b059f5646ce6
Byeongil Park <bryan1010110@gmail.com>
2024-10-30 22:33:52 -0400

    loop

--- Unit5_JavaScript/survey_test.js
+++ Unit5_JavaScript/survey_test.js
@@ -1,39 +1,7 @@
-//Given code
-const guests = {
-    ANTONY: {
-        title: 'General',
-        region: 'Rome',
-        dietaryPreperence: 'Vegitarian',
-        pastGifts: ['Golden Laurel', 'Chariot']
-    },
-    CICERO: {
-        title: 'Orator',
-        region: 'Arpinum',
-        dietaryPreperence: 'Omnivore',
-        pastGifts: ['Scroll of Proverbs', 'Quill']
-    }
-};
 
-// Step 1
-guests.BRUTUS = {
-    title: 'Senator',
-    region: 'Rome',
-    dietaryPreperence: 'Vegan',
-    pastGifts: ['Silver Dagger']
-};
+let a = ['H','E','L','L','O',' ','W','O','R','L','D']
 
-// Step 2
-guests.CICERO.pastGifts.push('Golden Lyre');
 
-// Step 3
-const regionAntony = guests.ANTONY.region;
-
-// Step 4
-delete guests.CICERO;
-
-// Step 5
-const generalProfile = guests.ANTONY;
-generalProfile.region = 'Egypt';
-
-// console.log(guests);
-console.log(regionAntony);
+for(let alpha of a) {
+    console.log(alpha);
+}
