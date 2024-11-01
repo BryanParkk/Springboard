00db11e41049997f4a734c1a4f2f6143b9b6adfd
Byeongil Park <bryan1010110@gmail.com>
2024-11-01 13:03:28 -0400

    for update

--- Unit5_JavaScript/survey_test.js
+++ Unit5_JavaScript/survey_test.js
@@ -1,2 +1,25 @@
+const animals = [ 'lions', 'cats', 'hippos', 'aligators', 'dogs'];
 
-let a = ['H','E','L','L','O',' ','W','O','R','L','D']
+for (let a=0; a<animals.length; a++) {
+    console.log(a, animals[a]);
+}
+
+console.log('-----------------------------------------------');
+const students = [
+    {
+        name: 'park',
+        score: 96
+    },
+    {
+        name: 'eunji',
+        score: 99
+    },
+    {
+        name: 'kim',
+        score: 92
+    }
+]
+
+for(let i=0; i<students.length; i++) {
+    console.log(i, students[i].name, students[i].score)
+}
--- Unit5_JavaScript/test.js
+++
@@ -1,4 +0,0 @@
-
-var a=7;
-
-a>9? console.log('YES'):console.log('NO');
