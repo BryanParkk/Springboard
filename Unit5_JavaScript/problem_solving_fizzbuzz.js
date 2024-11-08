3ebb3bbc191e5af41580b1dbbe070168e4179419
Byeongil Park <bryan1010110@gmail.com>
2024-11-08 17:59:46 -0500

    fizzbuzz update

--- Unit5_JavaScript/problem_solving_fizzbuzz.js
+++ Unit5_JavaScript/problem_solving_fizzbuzz.js
@@ -1,17 +1,40 @@
 // Problem solving - Practice
 
-// FizzBuzz
-// let n = Math.floor(Math.random() * 100); // 0 to 100 number
-let n = 30;
+// Try 1 //
 
-if(n % 3 == 0) {
-    console.log(`Fizz ${n}`);
-}else if(n % 5 == 0) {
-    console.log(`Buzz ${n}`);
-}else if(n % 3 == 0 && n % 5 == 0) {
-    console.log(`FizzBuzz ${n}`);
-}else {
-    console.log(n);
+// let n = Math.floor(Math.random() * 100); // 0 to 100 number
+// if(n % 3 == 0) {
+//     if(n % 5 == 0) {
+//     console.log(`FizzBuzz ${n}`);
+//     }else 
+//     console.log(`Fizz ${n}`);
+// }else if(n % 5 == 0) {
+//     console.log(`Buzz ${n}`);
+// }else if(n % 3 == 0 && n % 5 == 0) {
+//     console.log(`FizzBuzz ${n}`);
+// }else {
+//     console.log(n);
+// }
+
+
+// Try 2 //
+
+const n = 100;
+
+if (!Number.isInteger(n) || n < 1 || n >= 1000) {
+    console.log.out("Erro100");
 }
 
+for(let i=1; i<=n; i++) {
+    if(i % 3 == 0) {
+        if(i % 5 == 0) {
+        console.log(`FizzBuzz`);
+        }else 
+        console.log(`Fizz`);
+    }else if(i % 5 == 0) {
+        console.log(`Buzz`);
+    }else {
+        console.log(i);
+    }
+}
 
