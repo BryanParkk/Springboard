8154a7f0e9cad32f7fa2187feec12eef08ddd28b
Byeongil Park <bryan1010110@gmail.com>
2024-11-09 15:30:45 -0500

    practice 5.8 - 2 (Exercise 9) update

---
+++ Unit5_JavaScript/tech_survey_proj_5.8-2.js
@@ -0,0 +1,17 @@
+//Technical Survey Project - part 2 - Exercise 9
+
+//Starter code
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function encrypt (message, shiftValue)
+{
+  // Your encryption code here
+  return encryptedMessage;
+}
+
+function decrypt (encryptedMessage, shiftValue)
+{
+  // Your decryption code here
+  return decryptedMessage;
+}
