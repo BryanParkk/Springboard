5020f93568cdca2e40fedf3f84642e6716a50bb4
Byeongil Park <bryan1010110@gmail.com>
2024-11-09 15:26:10 -0500

    practice 5.8 final

--- Unit5_JavaScript/tech_survey_proj_5.8.js
+++ Unit5_JavaScript/tech_survey_proj_5.8.js
@@ -7,24 +7,61 @@ const alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
 
 // Step 1
-function encryptLetter(letter, shiftValue) {
-    if (typeof(friend) !== 'string') {
-        console.log('error');
-    }
-    const encLetter = alphabet[alphabet.indexOf(letter.toLowerCase()) + shiftValue % alphabet.length];
-    return encLetter;
+// function encryptLetter(letter, shiftValue) {
+//     if (typeof(friend) !== 'string') {
+//         console.log('error');
+//     }
+//     const encLetter = alphabet[alphabet.indexOf(letter.toLowerCase()) + shiftValue % alphabet.length];
+//     return encLetter;
+// }
+// console.log(encryptLetter('a', shiftValue));
+
+// Step 1
+function encryptLetter(letter, shift) {
+    const index = alphabet.indexOf(letter.toLowerCase());
+    const newIndex = (index + shift) % alphabet.length;
+    return alphabet[newIndex];
 }
-console.log(encryptLetter('a', shiftValue));
 console.log('---------------------------------------');
 
 // Step 2
-function encrtyptMessage(word, shiftValue) {
-    for(let sq of word) {
-        let encMsg = encryptLetter(sq, shiftValue);
-        console.log(encMsg);
+// function encrtyptMessage(word, shiftValue) {
+//     for(let sq of word) {
+//         let encMsg = encryptLetter(sq, shiftValue);
+//         console.log(encMsg);
+//     }
+// }
+// console.log(encrtyptMessage(friend, shiftValue));
+
+// Step 2
+function encryptMessage(word, shift) {
+    let encryptedMessage = "";
+    for (let i = 0; i < word.length; i++) {
+        encryptedMessage += encryptLetter(word[i], shift);
     }
+    return encryptedMessage;
 }
-console.log(encrtyptMessage(friend, shiftValue));
 console.log('---------------------------------------');
 
 // Step 3
+function decryptLetter(letter, shift) {
+    const index = alphabet.indexOf(letter.toLowerCase());
+    const newIndex = (index - shift + alphabet.length) % alphabet.length;
+    return alphabet[newIndex];
+}
+
+// Step 4
+function decryptMessage(word, shift) {
+    let decryptedMessage = "";
+    for (let i = 0; i < word.length; i++) {
+        decryptedMessage += decryptLetter(word[i], shift);
+    }
+    return decryptedMessage;
+}
+console.log('---------------------------------------');
+
+// Result
+console.log( encryptLetter('a', shiftValue) );
+console.log( encryptMessage(friend, shiftValue) );
+console.log( decryptLetter('d', shiftValue) );
+console.log( decryptMessage(encryptMessage(friend, shiftValue), shiftValue) );
