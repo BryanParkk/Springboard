80d57ab09dc3cbca794f386c08be14592a391720
Byeongil Park <bryan1010110@gmail.com>
2024-11-11 21:05:14 -0500

    practice

--- Unit5_JavaScript/tech_survey_proj_5.8-2.js
+++ Unit5_JavaScript/tech_survey_proj_5.8-2.js
@@ -1,18 +1,38 @@
 //Technical Survey Project - part 2 - Exercise 9
 
-//Starter code
-
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function encryptLetter(letter, shift) {
+  const index = alphabet.indexOf(letter.toLowerCase());
+  const newIndex = (index + shift) % alphabet.length;
+  return alphabet[newIndex];
+}
 
 function encrypt (message, shiftValue)
 {
-
+  let encryptedMessage = "";
+  for (let i = 0; i < message.length; i++) {
+      encryptedMessage += encryptLetter(message[i], shiftValue);
+  }
   return encryptedMessage;
+}
+///////////////////////////////////////////////////////////////////////////////
+
+function decryptLetter(letter, shift) {
+  const index = alphabet.indexOf(letter.toLowerCase());
+  const newIndex = (index - shift + alphabet.length) % alphabet.length;
+  return alphabet[newIndex];
 }
 
 function decrypt (encryptedMessage, shiftValue)
 {
-
+  let decryptedMessage = "";
+  for (let i = 0; i < encryptedMessage.length; i++) {
+      decryptedMessage += decryptLetter(encryptedMessage[i], shiftValue);
+  }
   return decryptedMessage;
 }
+///////////////////////////////////////////////////////////////////////////////
 
+console.log( encrypt('GARDEN', 3) );
+console.log( decrypt('jdughq', 3) );
