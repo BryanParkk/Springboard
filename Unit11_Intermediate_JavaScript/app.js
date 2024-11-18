9de68386c84a5f785c9b0b61edd6a5bc10f7ae80
Byeongil Park <bryan1010110@gmail.com>
2024-11-18 17:50:04 -0500

    update

---
+++ Unit11_Intermediate_JavaScript/app.js
@@ -0,0 +1,14 @@
+// Errors
+// - Syntax error
+// - Reference error
+// = Type error
+
+// Debugging
+console.log(1+3);
+console.log(1+3);
+console.log(1+3);
+console.log(1+3;);
+console.log(1+3);
+console.log(1+3);
+console.log(1+3);
+console.log(1+3);
