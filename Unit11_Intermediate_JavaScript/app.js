c54b044d492973385fa5932b243c5cfea6acc375
Byeongil Park <bryan1010110@gmail.com>
2024-11-21 23:07:06 -0500

    update

--- Unit11_Intermediate_JavaScript/app.js
+++ Unit11_Intermediate_JavaScript/app.js
@@ -6,3 +6,11 @@
 // Debugging
 // - do googling them
 // - googling, mentor, stack overflow
+
+let grades = [90, 98, 76, 54, 66, 90, 81];
+let sum = 0;
+debugger;
+for (let i = 0; i <= grades.length; i++) {
+    sum += grades[i];
+}
+let avg = sum / grades.length;
