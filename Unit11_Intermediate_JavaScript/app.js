005669eaf6c79ae8352c1d8e29ab4e660d48024d
Byeongil Park <bryan1010110@gmail.com>
2024-11-23 17:06:40 -0500

    update

--- Unit11_Intermediate_JavaScript/app.js
+++ Unit11_Intermediate_JavaScript/app.js
@@ -9,8 +9,27 @@
 
 try {
     functionThatDoesNotExist();
-} catch (err) {
-    console.log('Somthing went wrong', err);
+} catch (e) {
+    console.log('Somthing went wrong', e);
 }
 
 console.log("did we make it?");
+
+
+
+try {
+    3+3;
+} catch {
+    console.log("error");
+}
+console.log("success");
+
+
+
+try {
+    asdfasfdasdfsadf();
+} catch (e) {
+    console.log('Oh no error!!');
+    console.log(e);
+} 
+console.log('success')
