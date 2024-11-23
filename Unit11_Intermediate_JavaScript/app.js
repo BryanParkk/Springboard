56616e751bf71273873f16b17b6078f8a77cc9ba
Byeongil Park <bryan1010110@gmail.com>
2024-11-23 17:38:19 -0500

    error update

--- Unit11_Intermediate_JavaScript/app.js
+++ Unit11_Intermediate_JavaScript/app.js
@@ -1,3 +1,4 @@
+//////////////////////////////////////////////////////////
 // Errors
 // - Syntax error
 // - Reference error
@@ -6,30 +7,83 @@
 // Debugging
 // - do googling them
 // - googling, mentor, stack overflow
+//////////////////////////////////////////////////////////
+
+// try {
+//     functionThatDoesNotExist();
+// } catch (e) {
+//     console.log('Somthing went wrong', e);
+// }
+
+// console.log("did we make it?");
+
+
+
+// try {
+//     3+3;
+// } catch {
+//     console.log("error");
+// }
+// console.log("success");
+
+
+
+// try {
+//     asdfasfdasdfsadf();
+// } catch (e) {
+//     console.log('Oh no error!!');
+//     console.log(e);
+// } 
+// console.log('success')
+
+
+
+// function displayInitials(user) {
+//     let firstNameLetter = user.firstName[0].toUpperCase();
+//     let lastNameLetter = user.lastName[0].toUpperCase();
+//     return `hello ${firstNameLetter}.${lastNameLetter}`
+// }
+// displayInitials({firstname: 'Jason', lastName: "Momoa"});
+
+
+
+// function displayInitials(user) {
+//     let firstNameLetter;
+//     let lastNameLetter;
+//     try {
+//         firstNameLetter = user.firstName[0].toUpperCase();
+//         lastNameLetter = user.lastName[0].toUpperCase();
+//     } catch(e) {
+//         return "Invalid input!"+e;
+//     }
+//     return `Hello ${firstNameLetter}.${lastNameLetter}`;
+// }
+// console.log( displayInitials({ firstName: 'Jason' } ));
+
+
+
+// try {
+//     undefined.pop();
+//     console.log("INSIDE TRY!");
+// } catch(e) {
+//     console.log('OH NO ERROR!');
+//     console.log(e);
+// } finally {
+//     console.log("INSIDE FINALLY!");
+// }
+// console.log('did we make?');
+
+
 
 try {
-    functionThatDoesNotExist();
-} catch (e) {
-    console.log('Somthing went wrong', e);
+    throw new TypeError("I don't like you doing that");
+    // throw new Error('I AM THE ERROR');
+    // undefined.pop();
 }
-
-console.log("did we make it?");
-
-
-
-try {
-    3+3;
-} catch {
-    console.log("error");
+catch(e) {
+    // console.log('there was an error', e);
+    // console.dir(e);
+    console.log("what kind of error?", e.name);
+    console.log("what is the message?", e.message);
+    console.log("where did it happen?", e.stack);
 }
-console.log("success");
-
-
-
-try {
-    asdfasfdasdfsadf();
-} catch (e) {
-    console.log('Oh no error!!');
-    console.log(e);
-} 
-console.log('success')
