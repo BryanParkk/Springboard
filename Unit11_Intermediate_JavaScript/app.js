079be94ec011970ce641d98daaadd9d3bb29b5ee
Byeongil Park <bryan1010110@gmail.com>
2024-11-26 23:38:02 -0500

    update

--- Unit11_Intermediate_JavaScript/app.js
+++ Unit11_Intermediate_JavaScript/app.js
@@ -107,3 +107,12 @@ function getMonthName(mo) {
     }
 }
 
+function sum(nums) {
+    let total = 0;
+    for (let i = 0; i < nums.length; i++) {
+        total += nums[i];
+    }
+    return total;
+}
+
+console.log( sum(5) );
