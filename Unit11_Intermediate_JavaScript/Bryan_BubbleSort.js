143736eff63713d135ba458dcf48baf09bdf8817
Byeongil Park <bryan1010110@gmail.com>
2024-11-29 16:01:22 -0500

    update

--- Unit11_Intermediate_JavaScript/Bryan_BubbleSort.js
+++ Unit11_Intermediate_JavaScript/Bryan_BubbleSort.js
@@ -17,22 +17,19 @@ function bubbleSort1(arr) {
 
 function bubbleSort2(arr) {
     let count = 0;
-    for(let i=0; i<arr.length; i++) {
-        for(let j=0; j<arr.length-1; j++) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
             count++;
-            if(arr[j] > arr[j+1]) {
+            if(arr[j] > arr[j + 1]) {
                 let temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
-    console.log(arr);
     console.log("TOTAL COUNT :", count);
     return arr;
 }
-
-
 
 bubbleSort1([45,6,7,23,1,19,40,32,19,99,100,34,35,36,23,22,1,-9]);
 bubbleSort2([45,6,7,23,1,19,40,32,19,99,100,34,35,36,23,22,1,-9]);
