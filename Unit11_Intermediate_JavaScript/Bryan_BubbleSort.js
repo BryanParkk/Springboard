1338d03c17515d5e140329b7f41b338cf07c52e5
Byeongil Park <bryan1010110@gmail.com>
2024-11-29 15:54:53 -0500

    update

--- Unit11_Intermediate_JavaScript/Bryan_BubbleSort.js
+++ Unit11_Intermediate_JavaScript/Bryan_BubbleSort.js
@@ -1,20 +1,8 @@
-// function bubbleSort(arr) {
-//     for(let i=0; i<arr.length; i++) {
-//         for(let j=0; j<arr.length; j++) {
-//             console.log(arr);
-//             if(arr[j] > arr[j+1]) {
-//                 let temp = arr[j];
-//                 arr[j] = arr[j+1];
-//                 arr[j+1] = temp;
-//             }
-//         }
-//     }
-// }
-
-function bubbleSort2(arr) {
+function bubbleSort1(arr) {
+    let count = 0;
     for(let i=0; i<arr.length; i++) {
-        for(let j=0; j<arr.length-1; j++) {
-            console.log(arr);
+        for(let j=0; j<arr.length; j++) {
+            count++;
             if(arr[j] > arr[j+1]) {
                 let temp = arr[j];
                 arr[j] = arr[j+1];
@@ -22,9 +10,29 @@ function bubbleSort2(arr) {
             }
         }
     }
+    console.log(arr);
+    console.log("TOTAL COUNT: ", count);
+    return arr;
+}
+
+function bubbleSort2(arr) {
+    let count = 0;
+    for(let i=0; i<arr.length; i++) {
+        for(let j=0; j<arr.length-1; j++) {
+            count++;
+            if(arr[j] > arr[j+1]) {
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    console.log(arr);
+    console.log("TOTAL COUNT :", count);
+    return arr;
 }
 
 
 
-
-bubbleSort2([45,6,7,23,1,19]);
+bubbleSort1([45,6,7,23,1,19,40,32,19,99,100,34,35,36,23,22,1,-9]);
+bubbleSort2([45,6,7,23,1,19,40,32,19,99,100,34,35,36,23,22,1,-9]);
