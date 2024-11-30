0a2d480fc738064ed192c613a1208e3c1ae22edf
Byeongil Park <bryan1010110@gmail.com>
2024-11-30 11:57:20 -0500

    js sort update

---
+++ Unit11_Intermediate_JavaScript/Bryan_JsSort.js
@@ -0,0 +1,19 @@
+let numbers = [100, 60, 200, 1000];
+
+// console.log( numbers.sort() );
+console.log( numbers.sort((a,b) => a - b) );
+
+
+let instructors = [
+    {name: "Elie", favLang: "English"},
+    {name: "Joel", favzLang: "Python"},
+    {name: "Alissa", favLang: "JS"}
+]
+
+// console.log( instructors.sort() );
+console.log( instructors.sort((a,b) => {
+    if(a.name < b.name) return -1;
+    if(a.name > b.name) return 1;
+    return 0;
+})
+);
