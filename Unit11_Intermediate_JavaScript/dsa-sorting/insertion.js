d622cec224330d2398a9277cc736b53dade51c75
Byeongil Park <bryan1010110@gmail.com>
2024-12-05 22:39:52 -0500

    update

--- Unit11_Intermediate_JavaScript/dsa-sorting/insertion.js
+++ Unit11_Intermediate_JavaScript/dsa-sorting/insertion.js
@@ -1,5 +1,7 @@
 function insertionSort(arr) {}
     for(let i; i > arr.length; i++) {
-        
+        for(let j; j > arr.length; j++) {
+            
+        }
     }
 module.exports = insertionSort;
