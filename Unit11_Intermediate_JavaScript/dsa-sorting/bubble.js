38379bcfbdb71942df8c80ecfe942ecb98e6b054
Byeongil Park <bryan1010110@gmail.com>
2024-12-07 17:21:49 -0500

    selection update

--- Unit11_Intermediate_JavaScript/dsa-sorting/bubble.js
+++ Unit11_Intermediate_JavaScript/dsa-sorting/bubble.js
@@ -3,9 +3,10 @@ function bubbleSort(arr) {
         for(let i = 0; i < arr.length; i++) {
             let swap = 0;
             if (arr[i] > arr[i+1]) {
-                swap = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = swap;
+                //swap = arr[i];
+                //arr[i] = arr[i+1];
+                //arr[i+1] = swap;]
+                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
                 i++;
             }
         }
@@ -13,17 +14,17 @@ function bubbleSort(arr) {
     return arr;
 }
 
-bubbleSort([4, 20, 12, 10, 7, 9]); // [4, 7, 9, 10, 12, 20]
-bubbleSort([0, -10, 7, 4]); // [-10, 0, 4, 7]
-bubbleSort([1, 2, 3]); // [1, 2, 3]
-bubbleSort([]);
+console.log( bubbleSort([4, 20, 12, 10, 7, 9]) ); // [4, 7, 9, 10, 12, 20]
+console.log( bubbleSort([0, -10, 7, 4]) ); // [-10, 0, 4, 7]
+console.log( bubbleSort([1, 2, 3]) ); // [1, 2, 3]
+console.log( bubbleSort([]) );
 
 let nums = [
     4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34,
     23, 2, 453, 546, 75, 67, 4342, 32
 ];
 
-bubbleSort(nums); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67,
+console.log( bubbleSort(nums) ); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35, 43, 67,
                   // 75, 232, 232, 453, 546, 4342]
                   
 module.exports = bubbleSort;
--- Unit11_Intermediate_JavaScript/dsa-sorting/selection.js
+++ Unit11_Intermediate_JavaScript/dsa-sorting/selection.js
@@ -1,3 +1,31 @@
-function selectionSort() {}
+function selectionSort(arr) {
+    for(let i = 0; i < arr.length - 1; i++) {
+        let minIndex = i; // set minimum index
+
+        // compare two value
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        // set  minIndex to current minIndex
+        if (minIndex !== i) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+    return arr;
+}
+
+console.log( selectionSort([4, 20, 12, 10, 7, 9]));
+console.log( selectionSort([4, 20, 12, 10, 7, 9]) ); // [4, 7, 9, 10, 12, 20]
+console.log( selectionSort([0, -10, 7, 4]) ); // [-10, 0, 4, 7]
+console.log( selectionSort([1, 2, 3]) ); // [1, 2, 3]
+console.log( selectionSort([]) );
+let nums = [
+    4, 3, 5, 3, 43, 232, 4, 34, 232, 32, 4, 35, 34, 23, 2,
+    453, 546, 75, 67, 4342, 32
+];
+console.log( selectionSort(nums) ); // [2, 3, 3, 4, 4, 4, 5, 23, 32, 32, 34, 34, 35,
+                                //  43, 67, 75, 232, 232, 453, 546, 4342]
 
 module.exports = selectionSort;
