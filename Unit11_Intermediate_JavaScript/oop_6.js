9539b7ab4df1058dffde6f4792f8ff466c852770
Byeongil Park <bryan1010110@gmail.com>
2024-12-17 22:30:36 -0500

    update

---
+++ Unit11_Intermediate_JavaScript/oop_6.js
@@ -0,0 +1,21 @@
+//Inheritance between function constructor
+let Person = function (name, gender, birthYear) {
+  this.name = name;
+  this.gender = gender;
+  this.birthYear = birthYear;
+};
+Person.prototype.calcAge = function () {
+  let age = new Date().getFullYear() - this.birthYear;
+  console.log(age);
+};
+
+let john = new Person("John", "Male", 1983);
+
+//Employee function constructor
+let Employee = function (name, gender, birthYear, empId, salary) {
+  this.name = name;
+  this.gender = gender;
+  this.birthYear = birthYear;
+  this.empId = empId;
+  this.salary = salary;
+};
