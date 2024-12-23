e972bd908d48046543ecc820cd44f9641a24db9f
Byeongil Park <bryan1010110@gmail.com>
2024-12-22 20:51:48 -0500

    inherit update

--- Unit11_Intermediate_JavaScript/oop_6.js
+++ Unit11_Intermediate_JavaScript/oop_6.js
@@ -10,6 +10,8 @@ Person.prototype.calcAge = function () {
 };
 
 let john = new Person("John", "Male", 1983);
+let merry = new Person("merry", "Female", 1984);
+console.log(john);
 
 //Employee function constructor
 let Employee = function (name, gender, birthYear, empId, salary) {
@@ -19,5 +21,13 @@ let Employee = function (name, gender, birthYear, empId, salary) {
   this.empId = empId;
   this.salary = salary;
 };
+Employee.prototype.calcSalary = function () {
+  return this.salary * 12;
+};
+Employee.prototype.empDetails = function () {
+  console.log(this.name);
+  console.log(this.empID);
+};
 
-//////
+let mark = new Employee("Mark", "Male", 1983, 101, 12000);
+console.log(mark);
