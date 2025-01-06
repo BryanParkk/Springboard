46c9319f182a164118341d344e85fcd2c1082730
Byeongil Park <bryan1010110@gmail.com>
2025-01-05 19:17:48 -0500

    update

--- Unit11_Intermediate_JavaScript/oop_7.js
+++ Unit11_Intermediate_JavaScript/oop_7.js
@@ -1,3 +1,4 @@
+///
 let user = {
   name: "John",
   role: "admin",
@@ -11,3 +12,46 @@ let user = {
     //Logic
   },
 };
+
+///
+let john = {
+  name: "John",
+  birthYear: 1990,
+  gender: "Male",
+
+  calculateAge() {
+    return 2024 - this.birthYear;
+  },
+};
+
+/// Person function constructor (constructor using pascal case)
+let Person = function (name, gender, birthYear) {
+  this.name = name;
+  this.gender = gender;
+  this.birthYear = birthYear;
+  this.calcAge = function () {
+    let age = new Date().getFullYear() - this.birthYear;
+    console.log(age);
+  };
+};
+
+let jon = new Person("jon", "Male", 1986);
+jon.calcAge();
+console.log(jon);
+
+let merry = new Person("merry", "Female", 1983);
+merry.calcAge();
+console.log(merry);
+
+// let Car = function (tire, window, water) {
+//   this.tire = tire;
+//   this.window = window;
+//   this.water = water;
+//   this.engine = function () {
+//     let gas = "v8";
+//     console.log(gas);
+//   };
+// };
+
+// let BMW = new Car(4, 4, 2);
+// console.log(BMW);
