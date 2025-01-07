0c89f37a2d51d032b9363c999b4e4e2d424d52e0
Byeongil Park <bryan1010110@gmail.com>
2025-01-06 21:29:34 -0500

    update

--- Unit11_Intermediate_JavaScript/oop_8.js
+++ Unit11_Intermediate_JavaScript/oop_8.js
@@ -14,9 +14,41 @@ class Person {
     console.log(new Date().getFullYear() - this.birthYear);
   }
 }
+Person.prototype.greet = function () {
+  console.log("Good morning " + this.name);
+};
 
 let john = new Person("Park", 1986, "Male");
 console.log(john);
+john.greet();
 
-// 2. class expression
-//let Person = class {};
+let merry = new Person("merry", 1983, "Female");
+console.log(merry);
+
+// 1. classes cannot be hoisted
+// 2. classes are first class citizen
+// 3. classes are excueted in strict mode
+
+//getter , setter
+
+let john2 = {
+  name: "John",
+  birthYear: 1983,
+  AnnualSalary: 12000,
+
+  get getName() {
+    return this.name;
+  },
+
+  set setName(val) {
+    if (val.length < 4) {
+      alert("Name should be of atleast 4 characters!");
+    } else {
+      this.name = val;
+    }
+  },
+};
+
+console.log(john.getName);
+john2.setName = "john doe";
+console.log(john.getName);
