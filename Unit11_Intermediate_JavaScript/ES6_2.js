f1b37db366d277931fd7676b28bda19efb0ce93f
Byeongil Park <bryan1010110@gmail.com>
2025-01-09 17:37:19 -0500

    update

---
+++ Unit11_Intermediate_JavaScript/ES6_2.js
@@ -0,0 +1,27 @@
+function holler() {
+  console.log("HEY YOU!");
+}
+
+const whisper = function () {
+  console.log("pssssst I have a secret!");
+};
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+const mathFuncs = [add, subtract, multiply, divide];
+
+setTimeout(whisper, 4000);
