34ad9318fbb511efd741733d8bd9a0d46156369f
Byeongil Park <bryan1010110@gmail.com>
2025-01-12 10:37:19 -0500

    update map

--- Unit11_Intermediate_JavaScript/ES6_8.js
+++ Unit11_Intermediate_JavaScript/ES6_8.js
@@ -1,10 +1,56 @@
 //map function
 //examples
-const numbers = [1, 2, 3, 4, 5];
+// const numbers = [1, 2, 3, 4, 5];
 
-const squaredNumbers = numbers.map((value, index, array) => {
-  console.log(`now value: ${value}, index: ${index}, array: ${array}`);
-  return value * value;
+// const squaredNumbers = numbers.map((value, index, array) => {
+//   console.log(`now value: ${value}, index: ${index}, array: ${array}`);
+//   return value * value;
+// });
+
+// console.log(squaredNumbers);
+
+//
+const numbers = [23, 4, 25, 64, 23, -12];
+const negatives = numbers.map(function (num) {
+  return num * -1;
 });
 
-console.log(squaredNumbers);
+const doubles = numbers.map(function (num) {
+  console.log(num * 2);
+});
+
+const todos = [
+  {
+    id: 1,
+    text: "walk with dog",
+    priority: "high",
+  },
+  {
+    id: 2,
+    text: "eating with friend",
+    priority: "medium",
+  },
+  {
+    id: 3,
+    text: "sleep in the bed",
+    priority: "very high",
+  },
+  {
+    id: 4,
+    text: "talk with wife",
+    priority: "low",
+  },
+];
+
+//
+const todoText = todos.map(function (todo) {
+  return todo.text;
+});
+
+//
+const links = Array.from(document.querySelectorAll("a"));
+const urls = links.map(function (a) {
+  return a.href;
+});
+
+//
