e44d7be84ff3293c8dc6ee36d96079ec721d7c8e
Byeongil Park <bryan1010110@gmail.com>
2025-01-15 23:08:54 -0500

    update

--- Unit11_Intermediate_JavaScript/ES6_9.js
+++ Unit11_Intermediate_JavaScript/ES6_9.js
@@ -1,3 +1,5 @@
+console.log("Hello this is ES6_9.js");
+
 //filter
 const words = [
   "iasdflkagncvjvrieajsdklnwfdas",
@@ -6,12 +8,33 @@ const words = [
   "qwerqwreqwerqwer",
   "qwerasdfvczcxvzvsdawervfgbafwec",
   "jukijuhyujhyyjuuyjmh",
+  "byeongil",
+  "eunji",
 ];
 
-words.filter(function (word) {
+const longWords = words.filter(function (word) {
   if (word.length >= 20) {
     return true;
   } else {
     return false;
   }
 });
+
+const longWords2 = words.filter(function (word) {
+  return word.length >= 20;
+});
+
+const filterWords = words.filter(function (w) {
+  return w[0] === "z" || w[0] === "v";
+});
+
+const containsVowel = function (word) {
+  for (let char of word) {
+    if (isVowel(char)) return true;
+  }
+  return false;
+};
+
+const isVowel = function (char) {
+  return "aeiou".indexOf(char) != -1;
+};
