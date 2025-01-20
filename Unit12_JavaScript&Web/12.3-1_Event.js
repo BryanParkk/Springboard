73f2ca68632fa053bd94f3018add6932df66fda0
Byeongil Park <bryan1010110@gmail.com>
2025-01-19 23:05:54 -0500

    update event

---
+++ Unit12_JavaScript&Web/12.3-1_Event.js
@@ -0,0 +1,20 @@
+//Event
+
+//When a _______ event occurs on ________ elelment, do this _________
+//event: click, mouseover, mouseout, keydown, keyup, submit, etc.
+//element: button, input, form, etc.
+//action: change color, hide, show, etc.
+function makeBody(color) {
+  document.body.style.backgroundColor = color;
+}
+
+const btn = document.querySelector("#teal");
+btn.onclick = function () {
+  makeBody("teal");
+};
+
+//Event Listener
+const violetBtn = document.querySelector("#violet");
+violetBtn.addEventListener("click", function () {
+  makeBody("violet");
+});
