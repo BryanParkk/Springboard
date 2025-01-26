1848cfaab44a62613a72a4398c4651a565bb93ef
Byeongil Park <bryan1010110@gmail.com>
2025-01-25 23:33:49 -0500

    update map

---
+++ Unit11_Intermediate_JavaScript/11.12-1_adv_data_structure.js
@@ -0,0 +1,8 @@
+// const myMap = new Map();
+// myMap.set(7, "seven");
+// myMap.set("seven", "seven string");
+
+// const empty = [];
+// myMap.set(empty, "empty array");
+
+// console.log(myMap.get(7));
