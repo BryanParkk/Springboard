4953416034cee793c942c49f2bf552d22c01cfff
Byeongil Park <bryan1010110@gmail.com>
2025-01-28 16:20:43 -0500

    update

---
+++ Unit11_Intermediate_JavaScript/11.11_Modules.js
@@ -0,0 +1,17 @@
+//- Global scope
+/////- Module scope
+///////- Function scope
+//////////- Block scope - let and const
+console.log("hello");
+
+(function () {
+  var harry = "potter";
+  var voldmort = "He who must not be named";
+
+  function fight(char1, char2) {
+    var attack1 = Math.floor(Math.random() * char1.length);
+    var attack2 = Math.flood(Math.random() * char2.length);
+    return attack1 > attack2 ? `${char1} wins` : `${char2} wins`;
+  }
+  console.log(fight(harry, voldmort));
+});
--- Unit11_Intermediate_JavaScript/11.12-2_set.js
+++ Unit11_Intermediate_JavaScript/11.12-2_set.js
@@ -2,3 +2,30 @@
 const bannedHashTags = new Set(["nofilter", "justsaying", "winning", "yolo"]);
 
 bannedHashTags.add("bestlife").add("selfie");
+
+console.log(bannedHashTags.has("selfie"));
+
+bannedHashTags.delete("selfie");
+
+console.log(bannedHashTags.has("selfie"));
+
+//
+function filterHashTags1(tags) {
+  const bannedHashTags = new Set(["nofilter", "justsaying", "winning", "yolo"]);
+  return tags.filter((tag) => !bannedHashTags.has(tag));
+}
+const susansTags1 = ["happymonday", "yolo", "winning", "sunset"];
+//
+function filterHashTags2(tags) {
+  const bannedHashTags = ["nofilter", "justsaying", "winning", "yolo"];
+  return tags.filter((tag) => !bannedHashTags.includes(tag));
+}
+const susansTags2 = ["happymonday", "yolo", "winning", "sunset"];
+//
+
+const dup = ["aa", "ab", "ac", "ad", "aa", "ab", "ac"];
+const dup2 = new Set(dup);
+console.log(new Set(dup));
+console.log([...new Set(dup2)]);
+
+// set, add, has, delete, clear
