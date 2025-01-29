dd8e663b50974a97aa0e1d2609e8886fbb19bb8e
Byeongil Park <bryan1010110@gmail.com>
2025-01-28 23:20:06 -0500

    update

--- Unit11_Intermediate_JavaScript/11.11_Modules.js
+++ Unit11_Intermediate_JavaScript/11.11_Modules.js
@@ -42,7 +42,7 @@ const voldmort = "He who must not be named";
 export function jump() {
   // function
 }
-function fight2(char1, char2) {
+export default function fight2(char1, char2) {
   const attack1 = Math.floor(Math.random() * char1.length);
   const attack2 = Math.floor(Math.random() * char2.length);
   return attack1 > attack2 ? `${char1} wins` : `${char2} wins`;
