548944a001d07e0ddb262d289321c469f9c8ce9f
Byeongil Park <bryan1010110@gmail.com>
2025-01-29 17:33:46 -0500

    update module exercise 1

--- Unit11_Intermediate_JavaScript/11.11_Modules_Exercise/Exercise_1/inventory.mjs
+++ Unit11_Intermediate_JavaScript/11.11_Modules_Exercise/Exercise_1/inventory.mjs
@@ -1,1 +1,21 @@
-export function addItem() {}
+// inventory.mjs
+const inventory = [];
+
+// addItem()
+export function addItem(item) {
+  inventory.push(item);
+  console.log(`${item} added to inventory`);
+}
+
+// removeItem()
+export function removeItem(item) {
+  const index = inventory.indexOf(item);
+  inventory.splice(index, 1);
+  console.log(`${item} removed from inventory.`);
+}
+
+// listItems()
+export function listItems(item) {
+  console.log("<Current Inventory>");
+  inventory.forEach((item, index) => console.log(`${item}`));
+}
---
+++ Unit11_Intermediate_JavaScript/11.11_Modules_Exercise/Exercise_1/storeManager.mjs
@@ -0,0 +1,15 @@
+// storeManager.mjs
+import { addItem, removeItem, listItems } from "./inventory.mjs";
+
+// addItems
+addItem("Apple");
+addItem("Banana");
+addItem("Carot");
+addItem("Date");
+addItem("Egg");
+
+// removeItem
+removeItem("Date");
+
+// listItems
+listItems();
