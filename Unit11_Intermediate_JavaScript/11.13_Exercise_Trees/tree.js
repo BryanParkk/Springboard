6ddca6de3e9af2bfdd0ccd04d2734d6b69754591
Byeongil Park <bryan1010110@gmail.com>
2025-02-07 22:37:13 -0500

    update tree exercise and jest ...test ... npm run test

--- Unit11_Intermediate_JavaScript/11.13_Exercise_Trees/tree.js
+++ Unit11_Intermediate_JavaScript/11.13_Exercise_Trees/tree.js
@@ -6,30 +6,33 @@ class TreeNode {
     this.children = children;
   }
 }
-
 class Tree {
   constructor(root = null) {
     this.root = root;
   }
 
   /** sumValues(): add up all of the values in the tree. */
-
   sumValues() {
-    
+    if (!this.root) return 0;
+    let total = 0;
+    const toVisit = [this.root];
+    while (toVisit.length) {
+      const current = toVisit.pop();
+      total += current.val;
+      for (let child of current.children) {
+        toVisit.push(child);
+      }
+    }
+    return total;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
-
-  countEvens() {
-
-  }
+  countEvens() {}
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
-  numGreater(lowerBound) {
-
-  }
+  numGreater(lowerBound) {}
 }
 
 module.exports = { Tree, TreeNode };
