1a2056d81666220625b06bc53b84dda56fbc1cd4
Byeongil Park <bryan1010110@gmail.com>
2025-02-14 21:27:33 -0500

    update

--- Unit12_JavaScript&Web/12.4_Exercise/script.js
+++ Unit12_JavaScript&Web/12.4_Exercise/script.js
@@ -1,81 +1,91 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const noteContainer = document.getElementById("note-container");
-  const newNoteButton = document.getElementById("new-note-button");
-  const colorForm = document.getElementById("color-form");
-  const colorInput = document.getElementById("color-input");
+document.addEventListener("DOMContentLoaded", function ()
+{
+	const noteContainer = document.getElementById("note-container");
+	const newNoteButton = document.getElementById("new-note-button");
+	const colorForm = document.getElementById("color-form");
+	const colorInput = document.getElementById("color-input");
 
-  // TODO: Load the note color from the local storage.
-  let noteColor = localStorage.getItem("noteColor") || null; // Stores the selected note color from the form.
+	// TODO: Load the note color from the local storage.
+	let noteColor = localStorage.getItem("noteColor") || null; // Stores the selected note color from the form.
+	// TODO: Load the note ID counter from the local storage.
+	let noteIdCounter = Number(localStorage.getItem("noteIdCounter") || 0; // Counter for assigning unique IDs to new notes.
 
-  // TODO: Load the note ID counter from the local storage.
-  let noteIdCounter = Number(localStorage.getItem("noteIdCounter")) || 0; // Counter for assigning unique IDs to new notes.
-
-  // TODO: Load the notes from the local storage..
-
-  function addNewNote() {
-    const id = noteIdCounter;
-    const content = `Note ${id}`;
-
-    const note = document.createElement("textarea");
-    note.setAttribute("data-note-id", id.toString()); // Stores the note ID to its data attribute.
-    note.value = content; // Sets the note ID as value.
-    note.className = "note"; // Sets a CSS class.
-    note.style.backgroundColor = noteColor; // Sets the note's background color using the last selected note color.
-    noteContainer.appendChild(note); // Appends it to the note container element as its child.
-
-    noteIdCounter++; // Increments the counter since the ID is used for this note.
-
-    // TODO: Add new note to the saved notes in the local storage.
+	// TODO: Load the notes from the local storage.
+  function loadNotes() {
+    
   }
 
-  colorForm.addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevents the default event.
+	function addNewNote ()
+	{
+		const id = noteIdCounter;
+		const content = `Note ${id}`;
 
-    const newColor = colorInput.value.trim(); // Removes whitespaces.
+		const note = document.createElement("textarea");
+		note.setAttribute("data-note-id", id.toString()); // Stores the note ID to its data attribute.
+		note.value = content; // Sets the note ID as value.
+		note.className = "note"; // Sets a CSS class.
+		note.style.backgroundColor = noteColor; // Sets the note's background color using the last selected note color.
+		noteContainer.appendChild(note); // Appends it to the note container element as its child.
 
-    const notes = document.querySelectorAll(".note");
-    for (const note of notes) {
-      note.style.backgroundColor = newColor;
-    }
+		noteIdCounter++; // Increments the counter since the ID is used for this note.
 
-    colorInput.value = ""; // Clears the color input field after from submission.
+		// TODO: Add new note to the saved notes in the local storage.
+	}
 
-    noteColor = newColor; // Updates the stored note color with the new selection.
+	colorForm.addEventListener("submit", function (event)
+	{
+		event.preventDefault(); // Prevents the default event.
 
-    // TODO: Update the note color in the local storage.
-  });
+		const newColor = colorInput.value.trim();  // Removes whitespaces.
 
-  newNoteButton.addEventListener("click", function () {
-    addNewNote();
-  });
+		const notes = document.querySelectorAll(".note");
+		for (const note of notes)
+		{
+			note.style.backgroundColor = newColor;
+		}
 
-  document.addEventListener("dblclick", function (event) {
-    if (event.target.classList.contains("note")) {
-      event.target.remove(); // Removes the clicked note.
+		colorInput.value = ""; // Clears the color input field after from submission.
 
-      // TODO: Delete the note from the saved notes in the local storage.
-    }
-  });
+		noteColor = newColor; // Updates the stored note color with the new selection.
 
-  noteContainer.addEventListener(
-    "blur",
-    function (event) {
-      if (event.target.classList.contains("note")) {
-        // TODO: Update the note from the saved notes in the local storage.
-      }
-    },
-    true
-  );
+		// TODO: Update the note color in the local storage.
+	});
 
-  window.addEventListener("keydown", function (event) {
-    /* Ignores key presses made for color and note content inputs. */
-    if (event.target.id === "color-input" || event.target.type === "textarea") {
-      return;
-    }
+	newNoteButton.addEventListener("click", function ()
+	{
+		addNewNote();
+	});
 
-    /* Adds a new note when the "n" key is pressed. */
-    if (event.key === "n" || event.key === "N") {
-      addNewNote(); // Adds a new note.
-    }
-  });
+	document.addEventListener("dblclick", function (event)
+	{
+		if (event.target.classList.contains("note"))
+		{
+			event.target.remove(); // Removes the clicked note.
+
+			// TODO: Delete the note from the saved notes in the local storage.
+		}
+	});
+
+	noteContainer.addEventListener("blur", function (event)
+	{
+		if (event.target.classList.contains("note"))
+		{
+			// TODO: Update the note from the saved notes in the local storage.
+		}
+	}, true);
+
+	window.addEventListener("keydown", function (event)
+	{
+		/* Ignores key presses made for color and note content inputs. */
+		if (event.target.id === "color-input" || event.target.type === "textarea")
+		{
+			return;
+		}
+
+		/* Adds a new note when the "n" key is pressed. */
+		if (event.key === "n" || event.key === "N")
+		{
+			addNewNote(); // Adds a new note.
+		}
+	});
 });
