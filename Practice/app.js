7a7cf6ea263950134d264d2b80925e1cd0054e1e
Byeongil Park <bryan1010110@gmail.com>
2025-02-17 22:04:06 -0500

    update

--- Practice/app.js
+++ Practice/app.js
@@ -1,20 +1,20 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
+let name = "Apple";
+
+function myFunction() {
+  let num = 45;
+  console.log(name);
+  console.log(num);
 }
 
-const a = new Node("a");
-const b = new Node("b");
-const c = new Node("c");
-const d = new Node("d");
-const e = new Node("e");
-const f = new Node("f");
+myFunction();
 
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
+let student = {
+  firstName: "Bryan",
+  lastName: "Park",
+  age: 38,
+  mark: "brown eyes",
+};
+
+console.log(student);
+console.log(student.age);
+console.log(student.lastName);
