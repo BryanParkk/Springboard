4ccacf734392d5bd2ad118067db7096d4929877d
Byeongil Park <bryan1010110@gmail.com>
2025-02-23 21:35:29 -0500

    update

--- Unit13_JavaScript&APIs/13.2 REST API/giphy-party-starter/index.js
+++ Unit13_JavaScript&APIs/13.2 REST API/giphy-party-starter/index.js
@@ -1,3 +1,4 @@
 // look back at the <readme.md> file for some hints //
 // working API key //
-const giphyApiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+// const giphyApiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+const giphyApiKey = "kDgcKDfyaVueLYHVCkuL3gqxYHU0NapF";
