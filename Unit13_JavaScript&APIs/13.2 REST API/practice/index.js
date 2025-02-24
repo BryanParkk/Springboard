e087d6991ee033b1d708938cdd858ffedf2b9449
Byeongil Park <bryan1010110@gmail.com>
2025-02-23 19:18:07 -0500

    update

--- Unit13_JavaScript&APIs/13.2 REST API/practice/index.js
+++ Unit13_JavaScript&APIs/13.2 REST API/practice/index.js
@@ -1,3 +1,9 @@
 console.log(axios);
 
-axios.get("https://catfact.ninja/fact");
+axios.get("https://catfact.ninja/fact").then((response) => {
+  console.log(response.data.fact);
+});
+
+axios.get("https://dog.ceo/api/breeds/image/random").then((response) => {
+  console.log(response);
+});
