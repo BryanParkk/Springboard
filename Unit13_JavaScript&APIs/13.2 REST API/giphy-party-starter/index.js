c83262d0a916fea822778787e256bc4770a73f90
Byeongil Park <bryan1010110@gmail.com>
2025-02-23 23:19:19 -0500

    update

--- Unit13_JavaScript&APIs/13.2 REST API/giphy-party-starter/index.js
+++ Unit13_JavaScript&APIs/13.2 REST API/giphy-party-starter/index.js
@@ -5,24 +5,24 @@ const giphyApiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 const search_button = document.getElementById("search_button");
 const search_input = document.getElementById("search_input");
 const displayDiv = document.getElementById("gifs");
-
-async function giphyRequest() {
-  const response = await axios.get(
-    `http://api.giphy.com/v1/gifs/trending?api_key=${giphyApiKey}&limit=50`
-  );
-  console.log(response);
-  //   console.log(typeof response.status, response.status);
-  //   console.log(typeof response.data, response.data);
-  //   console.log(typeof response.data.data, response.data.data);
-  //   console.log(typeof response.data.data[0], response.data.data[0]);
-  //   console.log(response.data.data.length);
-  //   console.log(response.data.data[0].rating)
-}
+const remove_button = document.getElementById("remove_button");
+// async function giphyRequest() {
+//   const response = await axios.get(
+//     `http://api.giphy.com/v1/gifs/trending?api_key=${giphyApiKey}&limit=50`
+//   );
+//   console.log(response);
+//   //   console.log(typeof response.status, response.status);
+//   //   console.log(typeof response.data, response.data);
+//   //   console.log(typeof response.data.data, response.data.data);
+//   //   console.log(typeof response.data.data[0], response.data.data[0]);
+//   //   console.log(response.data.data.length);
+//   //   console.log(response.data.data[0].rating)
+// }
 
 search_button.addEventListener("click", function (event) {
   event.preventDefault();
   console.log("give me gif!");
-  const search_term = document.getElementById("search_input").value;
+  const search_term = document.getElementById("search_term").value;
   let newDiv = document.createElement("div");
   getGifs(search_term, newDiv);
 });
@@ -46,3 +46,7 @@ async function getGifs(search_term, newDiv) {
     displayDiv.appendChild(newDiv);
   }
 }
+
+remove_button.addEventListener("click", function () {
+  location.reload();
+});
