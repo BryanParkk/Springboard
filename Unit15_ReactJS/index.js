e0d70e08df4905435938555cefd85ba5cf4f4c4b
Byeongil Park <bryan1010110@gmail.com>
2025-03-15 23:38:31 -0400

    update

---
+++ Unit15_ReactJS/index.js
@@ -0,0 +1,8 @@
+// Front End Frameworks
+// - Larger JS libraries , methods
+// - provide "blueprint" for apps
+// - provide for code re-use, templating of HTML
+// - Opinionated, -> How you should design a JS app
+
+// Popular Front end Frameworks
+// - Angular , Ember, Vue , React
