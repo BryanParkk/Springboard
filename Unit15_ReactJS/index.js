e9870782cb5e0ef033e8b07cf9a9260ea83d7a23
Byeongil Park <bryan1010110@gmail.com>
2025-03-21 08:27:48 -0400

    update

--- Unit15_ReactJS/index.js
+++ Unit15_ReactJS/index.js
@@ -25,3 +25,11 @@ while (currentCard !== "spade") {
   currentCard = cards[Math.floor(Math.random() * 4)];
   console.log(currentCard);
 }
+//
+for (let i = 0; i < 99; i++) {
+  if (i > 2) {
+    break;
+  }
+  console.log("Banana.");
+}
+console.log("Orange you glad I broke out the loop!");
