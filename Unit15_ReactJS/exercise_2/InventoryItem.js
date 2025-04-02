e72309cacf05158796ac4d86031cf7c069df6e51
Byeongil Park <bryan1010110@gmail.com>
2025-04-02 18:44:04 -0400

    update exercise2

--- Unit15_ReactJS/exercise_2/InventoryItem.js
+++ Unit15_ReactJS/exercise_2/InventoryItem.js
@@ -1,10 +1,27 @@
-function InventoryItem (/* TODO: Take the props. Set defaults to the quantity. */)
-{
-	return (
-		<div>
-			<h2><!-- TODO: Render the item's details. --></h2>
-			<!-- TODO: Render the low stock alert based on the quantity of the item. -->
-			<!-- TODO: Render the high value alert based on the total value of the item. -->
-		</div>
-	);
+function InventoryItem({ name, type, quantity = 0, price = 0 }) {
+  const totalValue = quantity * price;
+  return (
+    <div className="item">
+      <h2>
+        {name} ({type})
+      </h2>
+      {/* stock alert*/}
+      {quantity < 5 && (
+        <message>
+          <span>
+            <p>⚠️ Low Stock! {quantity} remained.</p>
+          </span>
+        </message>
+      )}
+      {totalValue > 1000 && (
+        <message>
+          <span>
+            <p>💵 High value - consider extra protection!</p>
+          </span>
+        </message>
+      )}
+    </div>
+  );
 }
+
+export default InventoryItem;
--- Unit15_ReactJS/exercise_2/Message.js
+++ Unit15_ReactJS/exercise_2/Message.js
@@ -1,8 +1,7 @@
-function Message (/* TODO: Take the props. */)
-{
-	return (
-		<div>
-			<!-- TODO: Render children. -->
-		</div>
-	);
+function Message(/* TODO: Take the props. */ { props }) {
+  return (
+    <div>
+      <p className="message">{props.children}</p>
+    </div>
+  );
 }
