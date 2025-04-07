0402ffe87994c48c4cdaf60b739c9186a896702a
Byeongil Park <bryan1010110@gmail.com>
2025-04-07 14:58:43 -0400

    update

---
+++ Unit15_ReactJS/vite/vite_1/src/ShoppingList.jsx
@@ -0,0 +1,14 @@
+function ShopppingList() {
+  return (
+    <div>
+      <h2>Shopping List</h2>
+      <ul>
+        <li>Milk</li>
+        <li>Eggs</li>
+        <li>Bagles</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ShopppingList;
