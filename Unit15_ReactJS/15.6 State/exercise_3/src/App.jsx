be4c7e6cca33b2e0a639405051e0c4281c4ec5be
Byeongil Park <bryan1010110@gmail.com>
2025-04-23 23:26:39 -0400

    update

--- Unit15_ReactJS/15.6 State/exercise_3/src/App.jsx
+++ Unit15_ReactJS/15.6 State/exercise_3/src/App.jsx
@@ -1,13 +1,53 @@
 import { useState } from 'react'
 import './App.css'
 
+// function App() {
+//   const [number, setNumber] = useState(0)
+
+//   const increase = () => {
+//     setNumber(number + 1);
+//   }
+
+//   const increaseAsync = () => {
+//     setTimeout(() => {
+//       setNumber((prev) => prev + 1);
+//     }, 2000)
+//   }
+
+//   return (
+//     <div>
+//       <button onClick={increase}>Increase</button>
+//       <button onClick={increaseAsync}>Increase Async</button>
+//       <h1>{number}</h1>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  const [user, setUser] = useState({
+    name: "john",
+    email: "john@gmail.com",
+    images: ["profile.png", "cover.png"],
+  });
 
-  return (
-    <>
+  const changeUser = () => {
+    setUser((prev) => ({...prev, name:input}));
+  };
 
-    </>
+  return(
+    <div>
+      <h2> User: </h2>
+      <input
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="enter a new name"
+      />
+      <button onClick={changeUser}>Change username</button>
+      <span>Username is: {user.name}</span>
+    </div>
   )
 }
 
