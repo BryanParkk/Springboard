9872161812407816f8279a30906cd355a8712cef
Byeongil Park <bryan1010110@gmail.com>
2025-04-26 00:00:53 -0400

    update

--- Unit15_ReactJS/15.6 State/exercise_2-1/src/Circle.jsx
+++ Unit15_ReactJS/15.6 State/exercise_2-1/src/Circle.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Circle.css";
 
-const Circle = ( {color, idx, x, y, changePosition} ) => {
+function getRandom(min = 0, max = 100) {
+    return Math.random() * (max - min) + min;
+}
+
+const Circle = ( { color, idx } ) => {
+    const [pos, setPos] = useState( { x: getRandom(), y: getRandom() } )
+    const moveCircle = () => {
+        setPos({ x: getRandom(), y: getRandom() })
+    }
     return (
         <div
-            onClick={() => changePosition(idx)}
+            onClick={moveCircle}
             className="Circle"
             style={{
                 backgroundColor: color,
                 position: 'absolute',
-                top: `${y}vh`,
-                left: `${x}vw`
+                top: `${pos.y}vh`,
+                left: `${pos.x}vw`
             }}
         >
             {idx + 1}
