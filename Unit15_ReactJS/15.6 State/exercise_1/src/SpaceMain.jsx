acd8603112aa5fefac23c7489319c0e1f972898c
Byeongil Park <bryan1010110@gmail.com>
2025-04-28 20:10:29 -0400

    update 1

--- Unit15_ReactJS/15.6 State/exercise_1/src/SpaceMain.jsx
+++ Unit15_ReactJS/15.6 State/exercise_1/src/SpaceMain.jsx
@@ -1,9 +1,33 @@
 import React, { useState } from 'react';
 import SpaceBottom from './SpaceBottom'
+// import SpaceshipBattle from './SpaceshipBattle'
 
 const SpaceMain = () => {
-    const a = 'Bryan';
-    console.log(`Hello World, ${a}`);
+    const [playerHp, setPlayerHp] = useState(100);
+    const [enemyHp, setEnemyHp] = useState(100);
+    const playerRandomHp = Math.floor(Math.random() * 30) + 1;
+    const enemyRandomHp = Math.floor(Math.random() * 30) + 1;
+    
+    //
+    const [btnText, setBtnText] = useState("Fire!");
+
+    //
+    const handleFire = () => {
+        if (playerHp > 0 && enemyHp > 0) {
+            // 둘 다 살아있을 때만 공격
+            setPlayerHp(prev => {
+              const newHp = Math.max(prev - playerRandomHp, 0);
+              if (newHp === 0 || enemyHp - enemyRandomHp <= 0) {
+                setBtnText('Restart?');
+              }
+              return newHp;
+            });
+        
+            setEnemyHp(prev => Math.max(prev - enemyRandomHp, 0));
+          } else {
+            setBtnText('Restart?');
+          }
+        };
 
     return (
         <div className="space-area">
@@ -14,14 +38,14 @@ const SpaceMain = () => {
             <div className="mid">
                 <div className="player-info">
                     <label className="label-player-name">Player Health: </label>
-                    <label className="label-player-hp">0</label>
+                    <label className="label-player-hp">{playerHp}</label>
                 </div>
 
-                <button className="btn-battle">Fire!</button>
+                <button className="btn-battle" onClick={handleFire}>{btnText}</button>
 
                 <div className="enemy-info">
                     <label className="label-enemy-name">Enemy Health: </label>
-                    <label className="label-enemy-hp">0</label>
+                    <label className="label-enemy-hp">{enemyHp}</label>
                 </div>
             </div>
 
---
+++ Unit15_ReactJS/15.6 State/exercise_1/src/SpaceshipBattle.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// function getRandom(min = 0, max = 30) {
+//     return Math.floor(Math.random() * (max - min) + 1);
+// }
+
+// const SpaceshipBattle = (props) => {
+//     const { player_hp, enemy_hp } = props;
+
+//     return (
+//         <div>
+//             {player_hp}{enemy_hp}
+//         </div>
+//     );
+// }
