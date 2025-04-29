2c1c499d3102fac52e887812feb97cf2111dc263
Byeongil Park <bryan1010110@gmail.com>
2025-04-28 21:20:23 -0400

    update

--- Unit15_ReactJS/15.6 State/exercise_1/src/SpaceMain.jsx
+++ Unit15_ReactJS/15.6 State/exercise_1/src/SpaceMain.jsx
@@ -1,82 +1,13 @@
-import React, { useState } from 'react';
-import SpaceBottom from './SpaceBottom'
-// import SpaceshipBattle from './SpaceshipBattle'
+import React from 'react';
+import SpaceshipBattle from './SpaceshipBattle';
 
 const SpaceMain = () => {
-    const [playerHp, setPlayerHp] = useState(100);
-    const [enemyHp, setEnemyHp] = useState(100);
-    const playerRandomHp = Math.floor(Math.random() * 30) + 1;
-    const enemyRandomHp = Math.floor(Math.random() * 30) + 1;
-    
-    //
-    const [btnText, setBtnText] = useState("Fire!");
-
-    //
-    const [bottomText, setBottomText] = useState('⚠️ Engage the enemy ⚠️');
-
-    // handle fire
-    const handleFire = () => {
-        if(playerHp > 0 && enemyHp > 0) {
-            const playerNew = Math.max(playerHp - playerRandomHp, 0);
-            const enemyNew = Math.max(enemyHp - enemyRandomHp, 0);
-
-            setPlayerHp(playerNew);
-            setEnemyHp(enemyNew);
-
-            if (playerNew === 0 || enemyNew === 0) {
-                setBtnText('Restart?');
-
-                if(playerNew === 0 && enemyNew === 0) {
-                    setBottomText(`‼️ It's a draw! Both spacecrafts have been neutralized. ‼️`);
-                } else if (playerNew === 0) {
-                    setBottomText(`Mission Failed. Your spacecraft has been defeated. 😨`);
-                } else if (enemyNew === 0) {
-                    setBottomText(`🎉 Congratulations! You've successfully defended your spacecraft! 🎉`);
-                }
-            }
-        }
-    };
-
-    // handle Restart
-    const handleRestart = () => {
-        setPlayerHp(100);
-        setEnemyHp(100);
-        setBtnText('Fire!');
-        setBottomText('⚠️ Engage the enemy ⚠️');
-    };
-
-    // control handle
-    const handleButtonClick = () => {
-        if(btnText === 'Fire!') {
-            handleFire();
-        } else {
-            handleRestart();
-        }
-    };
-
     return (
         <div className="space-area">
             <div className="top">
                 <h1 className="title"> 🚀 Space Battle Simulator 🚀 </h1>
             </div>
-
-            <div className="mid">
-                <div className="player-info">
-                    <label className="label-player-name">🛰️ Player Health: </label>
-                    <label className="label-player-hp">{playerHp}</label>
-                </div>
-
-                <button className={btnText === 'Fire!' ? 'fire-btn' : 'restart-btn'} onClick={handleButtonClick}>{btnText}</button>
-
-                <div className="enemy-info">
-                    <label className="label-enemy-name">🛸 Enemy Health: </label>
-                    <label className="label-enemy-hp">{enemyHp}</label>
-                </div>
-            </div>
-
-            <div className="bottom">
-                <SpaceBottom text={bottomText}/>
-            </div>
+            <SpaceshipBattle />
         </div>
     )
 }
--- Unit15_ReactJS/15.6 State/exercise_1/src/SpaceshipBattle.jsx
+++ Unit15_ReactJS/15.6 State/exercise_1/src/SpaceshipBattle.jsx
@@ -1,15 +1,78 @@
-// import React from 'react'
+import React, { useState } from 'react';
+import SpaceBottom from './SpaceBottom'
 
-// function getRandom(min = 0, max = 30) {
-//     return Math.floor(Math.random() * (max - min) + 1);
-// }
+const SpaceshipBattle = () => {
+    const [playerHp, setPlayerHp] = useState(100);
+    const [enemyHp, setEnemyHp] = useState(100);
+    const playerRandomHp = Math.floor(Math.random() * 30) + 1;
+    const enemyRandomHp = Math.floor(Math.random() * 30) + 1;
+    
+    //button
+    const [btnText, setBtnText] = useState("Fire!");
 
-// const SpaceshipBattle = (props) => {
-//     const { player_hp, enemy_hp } = props;
+    //bottom
+    const [bottomText, setBottomText] = useState('⚠️ Engage the enemy ⚠️');
 
-//     return (
-//         <div>
-//             {player_hp}{enemy_hp}
-//         </div>
-//     );
-// }
+    //handle fire
+    const handleFire = () => {
+        if(playerHp > 0 && enemyHp > 0) {
+            const playerNew = Math.max(playerHp - playerRandomHp, 0);
+            const enemyNew = Math.max(enemyHp - enemyRandomHp, 0);
+            
+            setPlayerHp(playerNew);
+            setEnemyHp(enemyNew);
+
+            if (playerNew === 0 || enemyNew === 0) {
+                setBtnText('Restart?');
+
+                if(playerNew === 0 && enemyNew === 0) {
+                    setBottomText(`‼️ It's a draw! Both spacecrafts have been neutralized. ‼️`);
+                } else if (playerNew === 0) {
+                    setBottomText(`Mission Failed. Your spacecraft has been defeated. 😨`);
+                } else if (enemyNew === 0) {
+                    setBottomText(`🎉 Congratulations! You've successfully defended your spacecraft! 🎉`);
+                }
+            }
+        }
+    };
+
+    //handle Restart
+    const handleRestart = () => {
+        setPlayerHp(100);
+        setEnemyHp(100);
+        setBtnText('Fire!');
+        setBottomText('⚠️ Engage the enemy ⚠️');
+    };
+
+    // control handle
+    const handleButtonClick = () => {
+        if(btnText === 'Fire!') {
+            handleFire();
+        } else {
+            handleRestart();
+        }
+    };
+
+    return (
+        <>
+            <div className="mid">
+                <div className="player-info">
+                    <label className="label-player-name">🛰️ Player Health: </label>
+                    <label className="label-player-hp">{playerHp} ❤️</label>
+                </div>
+
+                <button className={btnText === 'Fire!' ? 'fire-btn' : 'restart-btn'} onClick={handleButtonClick}>{btnText}</button>
+
+                <div className="enemy-info">
+                    <label className="label-enemy-name">🛸 Enemy Health: </label>
+                    <label className="label-enemy-hp">{enemyHp} ❤️</label>
+                </div>
+            </div>
+
+            <div className="bottom">
+                <SpaceBottom text={bottomText}/>
+            </div>
+        </>
+    )
+}
+export default SpaceshipBattle;
