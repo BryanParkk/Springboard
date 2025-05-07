590f1a0cc0410c9ddd4098d10c01f5abeb8987f4
Byeongil Park <bryan1010110@gmail.com>
2025-05-07 12:56:09 -0400

    update

---
+++ Unit15_ReactJS/15.9 Form/practice_1/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import UserForm from './UserForm'
+
+function App() {
+  return (
+    <>
+      <UserForm></UserForm>
+    </>
+  )
+}
+
+export default App
