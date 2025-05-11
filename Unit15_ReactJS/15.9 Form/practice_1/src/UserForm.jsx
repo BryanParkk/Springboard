5cbd368fdbce20dfd5356db38eae5160526c5031
Byeongil Park <bryan1010110@gmail.com>
2025-05-10 20:59:07 -0400

    update

--- Unit15_ReactJS/15.9 Form/practice_1/src/UserForm.jsx
+++ Unit15_ReactJS/15.9 Form/practice_1/src/UserForm.jsx
@@ -1,35 +1,58 @@
 import React, { useState } from 'react'
 
-const UserForm = () => {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
+    const UserForm = () => {
+        const [username, setUsername] = useState("");
+        const [email, setEmail] = useState("");
+        const [formData, setFormData] = useState({
+            username: "",
+            email: ""
+        })
 
-    const handleChange = (e) => {
-        setUsername(e.target.value)
+    const handleChange = e => {
+        console.log(e.target.name);
+        console.log(e.target.value);
     }
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value)
-    }
+    // const handleChange = (e) => {
+    //     setUsername(e.target.value)
+    // }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        alert(`Created user, ${username} w/ email ${email}`)
-        setUsername("");
-        setEmail("");
-    }
+    // const handleEmailChange = (e) => {
+    //     setEmail(e.target.value)
+    // }
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     alert(`Created user, ${username} w/ email ${email}`)
+    //     setUsername("");
+    //     setEmail("");
+    // }
 
     
     return (
-        <form onSubmit={handleSubmit}>
+        // <form onSubmit={handleSubmit}>
+        <form>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" placeholder="Enter your username" value={username} onChange={handleChange} />
+            <input 
+                id="username" 
+                type="text" 
+                name="username"
+                placeholder="Enter your username" 
+                value={formData.username} 
+                onChange={handleChange} />
             <br></br>
             <label htmlFor="email">Email</label>
-            <input id="email" type="email" placeholder="Your Email address here" value={email} onChange={handleEmailChange} />
+            <input 
+                id="email" 
+                type="email"
+                name="email"
+                placeholder="Your Email address here" 
+                value={formData.email} 
+                onChange={handleChange} />
             <button>Add me to list!!@</button>
         </form>
     )
 }
 
 export default UserForm;
+ 
