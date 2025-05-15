ef0ab26c8ac55d323a76cf1c4dfcd5c7279d67b3
Byeongil Park <bryan1010110@gmail.com>
2025-05-15 18:11:31 -0400

    update 5.10

---
+++ Unit15_ReactJS/15.10 Routing/practice_1/src/pages/help/Faq.jsx
@@ -0,0 +1,27 @@
+export default function Faq() {
+    return (
+        <div className="faq">
+            <h3>Frequently Asked Questions</h3>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere ipsa illum, porro aspernatur autem corrupti fuga iure nulla molestias excepturi tempore repellendus maiores exercitationem dignissimos aperiam magnam quis sapiente similique eveniet, pariatur quod nam? Expedita esse itaque consequatur quis commodi maxime, provident molestias est doloremque earum. Asperiores enim commodi nobis!</p>
+            </div>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere ipsa illum, porro aspernatur autem corrupti fuga iure nulla molestias excepturi tempore repellendus maiores exercitationem dignissimos aperiam magnam quis sapiente similique eveniet, pariatur quod nam? Expedita esse itaque consequatur quis commodi maxime, provident molestias est doloremque earum. Asperiores enim commodi nobis!</p>               
+            </div>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere ipsa illum, porro aspernatur autem corrupti fuga iure nulla molestias excepturi tempore repellendus maiores exercitationem dignissimos aperiam magnam quis sapiente similique eveniet, pariatur quod nam? Expedita esse itaque consequatur quis commodi maxime, provident molestias est doloremque earum. Asperiores enim commodi nobis!</p>
+            </div>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere ipsa illum, porro aspernatur autem corrupti fuga iure nulla molestias excepturi tempore repellendus maiores exercitationem dignissimos aperiam magnam quis sapiente similique eveniet, pariatur quod nam? Expedita esse itaque consequatur quis commodi maxime, provident molestias est doloremque earum. Asperiores enim commodi nobis!</p>
+            </div>
+        </div>
+    )
+}
