97985b1ce8c842ac677c4f3e13e669452a8f8ade
Byeongil Park <bryan1010110@gmail.com>
2025-05-15 17:37:22 -0400

    update 15.9

---
+++ Unit15_ReactJS/15.10 Routing/practice_1/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+    return (
+        <div className="home">
+            <h2>Welcome</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi velit tempora sit impedit in dolor facilis fugit dolores accusantium magnam ab veritatis enim fuga ipsam, sint iusto mollitia minima hic.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt praesentium exercitationem enim ad veniam ullam, esse perferendis nemo obcaecati minus est vel asperiores odio incidunt, consequatur blanditiis debitis quo molestiae.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque laboriosam recusandae non a explicabo dolore quam, quae adipisci totam vero tempore obcaecati enim nulla? Cumque, rerum! Autem recusandae ab esse?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum fugiat vitae repellat deleniti iste nulla, quod magni aperiam modi asperiores esse perspiciatis eos tempore quo quae? Unde vel culpa eum.</p>
+        </div>
+    )
+}
+
+export default Home;
