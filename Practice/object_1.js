c3bbee4574e1cb530ca27e74c3f73a31f42370f7
Byeongil Park <bryan1010110@gmail.com>
2025-05-15 13:41:47 -0400

    update

---
+++ Practice/object_1.js
@@ -0,0 +1,20 @@
+let spaceShip = {
+  homePlanet: "earth",
+  color: "red",
+};
+
+let tryReassignment = (obj) => {
+  obj = {
+    identified: false,
+    "transport type": "flying",
+  };
+
+  console.log(obj);
+};
+
+// spaceShip = {
+//   identified: false,
+//   "transport type": "flying",
+// };
+
+console.log(spaceShip);
