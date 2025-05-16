616aa1b1440b191794b82f901ad694909714cfce
Byeongil Park <bryan1010110@gmail.com>
2025-05-15 23:58:36 -0400

    update 15.10-6

--- Unit15_ReactJS/15.10 Routing/practice_1/src/App(createBrowserRouter).jsx
+++ Unit15_ReactJS/15.10 Routing/practice_1/src/App(createBrowserRouter).jsx
@@ -12,11 +12,9 @@ import About from './pages/About'
 // layouts
 import RootLayout from './layouts/RootLayout'
 import HelpLayout from './layouts/HelpLayout'
-import CareersLayout from './layouts/CareersLayout'
 import Faq from './pages/help/Faq'
 import Contact from './pages/help/Contact'
 import NotFound from './pages/NotFound'
-import Careers, { careersLoader } from './pages/Careers'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,14 +24,6 @@ const router = createBrowserRouter(
       <Route path="help" element={<HelpLayout />} >
         <Route path="faq" element={<Faq />} />
         <Route path="contact" element={<Contact />} />
-      </Route>
-
-      <Route path="careers" element={<CareersLayout />}>
-        <Route 
-          index 
-          element={<Careers />} 
-          loader={careersLoader}
-          /> 
       </Route>
 
       <Route path="*" element={<NotFound />} />
