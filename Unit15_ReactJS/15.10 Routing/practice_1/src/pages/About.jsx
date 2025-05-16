c44732998a6b661b8f3bdcbdab5b3b6cb6313c05
Byeongil Park <bryan1010110@gmail.com>
2025-05-16 02:02:55 -0400

    update 5.10 final

--- Unit15_ReactJS/15.10 Routing/practice_1/src/pages/About.jsx
+++ Unit15_ReactJS/15.10 Routing/practice_1/src/pages/About.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
+import { Navigate } from 'react-router-dom'
+
 function About() {
+    const [user, setUser] = useState('bryan');
+
+    if(!user) {
+        return <Navigate to="/" replace={true}/>
+    }
+
     return (
         <div className="about">
             <h2>About Us</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, ullam deleniti possimus suscipit consequatur accusantium nulla unde. Labore quod sequi provident nesciunt repudiandae quo ipsum, fugit voluptas nihil! Tenetur, commodi.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque similique, deserunt totam facere perspiciatis beatae mollitia laboriosam obcaecati nihil voluptatibus soluta exercitationem sunt porro quasi quod dolor incidunt eius repellendus.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet odio totam labore id enim! Harum quo quisquam quasi repellendus tempore libero magnam aliquid molestiae architecto totam. Repellendus dignissimos id eaque quibusdam? Modi consequatur nulla optio iure. Laudantium architecto, quisquam minima assumenda expedita modi quo est quasi doloremque nihil veniam corporis dicta quam aspernatur ea amet repudiandae similique. Voluptate veritatis unde optio eveniet tempora exercitationem perferendis omnis sed quidem sequi consequuntur itaque quia expedita hic dolorum nemo consequatur magni iste vero amet totam, nisi eum cumque fugiat. Quae ex porro maiores voluptatibus, molestiae dicta cupiditate hic quisquam recusandae expedita repudiandae laboriosam reprehenderit aliquam doloremque eos fugiat ipsa accusantium deserunt! Aperiam fugit ipsa corporis adipisci animi praesentium, iure ratione reiciendis vero sunt illo, qui nam ea eveniet veritatis at quibusdam quisquam recusandae. Tempore debitis quam velit suscipit soluta doloremque odio, fugit officia adipisci necessitatibus nihil nam molestiae, animi, quibusdam blanditiis eveniet. Eveniet sed dicta illum nam deserunt perspiciatis consectetur porro atque velit itaque fugiat et sint perferendis enim accusantium molestias saepe modi ut illo pariatur qui, cum eum distinctio. Laudantium maxime quia architecto rem possimus natus repellat veniam optio saepe deleniti harum pariatur vero fuga aliquam cupiditate voluptatum, exercitationem neque quisquam dicta!</p>
+
+            <button onClick={() => setUser(null)}>Logout</button>
         </div>
     )
 }
