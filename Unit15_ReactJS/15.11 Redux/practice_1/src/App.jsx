a8b40063dde2161d8d4d34067caa2dded64cf36e
Byeongil Park <bryan1010110@gmail.com>
2025-05-20 22:29:07 -0400

    update

--- Unit15_ReactJS/15.11 Redux/practice_1/src/App.jsx
+++ Unit15_ReactJS/15.11 Redux/practice_1/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <main className="App">
+
+    </main>
   )
 }
-
 export default App
