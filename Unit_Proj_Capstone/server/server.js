40cf79b7961e0948b0429f5dca87c61510f5bd6e
Byeongil Park <bryan1010110@gmail.com>
2025-07-09 00:01:21 -0400

    update

--- Unit_Proj_Capstone/server/server.js
+++ Unit_Proj_Capstone/server/server.js
@@ -8,8 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/exercises", exercisesRoute);
-app.use("/api/meals", mealsRoute);
+app.use("/api/exercises", exercisesRoute); // exercise
+app.use("/api/meals", mealsRoute); // mealplan
 const PORT = 5001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
