e371e6d9ca350b8eeed1f41004268d75747763bc
Byeongil Park <bryan1010110@gmail.com>
2025-07-08 23:52:58 -0400

    update

--- Unit_Proj_Capstone/client/src/features/pages/Settings.jsx
+++ Unit_Proj_Capstone/client/src/features/pages/Settings.jsx
@@ -1,4 +1,9 @@
 // src/pages/Settings.jsx
 export default function Settings() {
-  return <h1>Settings Page</h1>;
+  return (
+    <div>
+      <h1>&nbsp;Settings Page</h1>
+      <p>&nbsp;<button>Enable</button> Dark Mode</p>
+    </div>
+  );
 }
