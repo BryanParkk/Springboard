a842444874267ad4b0a9bd8b496dd5651e1e4d2f
Byeongil Park <bryan1010110@gmail.com>
2025-08-04 21:48:43 -0400

    update

---
+++ Practice/React_CodingDevil/practice-1/src/component/Hello.js
@@ -0,0 +1,11 @@
+import World from "./World";
+
+const Hello = () => {
+  return (
+    <h1>
+      Hello <World />
+    </h1>
+  );
+};
+
+export default Hello;
---
+++ Practice/React_CodingDevil/practice-1/src/component/Welcome.js
@@ -0,0 +1,5 @@
+const Welcome = () => {
+  return <p>Welcome to my homepage.</p>;
+};
+
+export default Welcome;
