b262f545324720dbd6861e0caaab585fce95697f
Byeongil Park <bryan1010110@gmail.com>
2025-08-06 23:29:08 -0400

    update

--- Practice/React_CodingDevil/practice-1/src/App.js
+++ Practice/React_CodingDevil/practice-1/src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./component/Header";
+import DayList from "./component/DayList";
+import Day from "./component/Day";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day" element={<DayList />} /> {/* 추가 */}
+          <Route path="/day/:day" element={<Day />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
