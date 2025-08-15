593ea583ebd4e5b1628ee34ad27b274e16285967
Byeongil Park <bryan1010110@gmail.com>
2025-08-15 17:46:13 -0400

    update final JWT

--- Unit19_Node.js/19.6 Authentication/practice-1/app.js
+++ Unit19_Node.js/19.6 Authentication/practice-1/app.js
@@ -1,8 +1,12 @@
+// To start
+// Terminal 1 :  nodemon app
+// Web browser : http://localhost:3000, /login, /logout, /signup, /smoothe
+
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 const cookieParser = require("cookie-parser");
-const { requireAuth } = require("./middleware/authMiddleware");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
 
 const app = express();
 
@@ -15,7 +19,6 @@ app.use(cookieParser());
 app.set("view engine", "ejs");
 
 // database connection
-// const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
 const dbURI =
   "mongodb+srv://bryan:1234%21%40%23%24@springboard.mtzjsej.mongodb.net/?retryWrites=true&w=majority&appName=SpringBoard";
 
@@ -29,23 +32,7 @@ mongoose
   .catch((err) => console.log(err));
 
 // routes
+app.get("*", checkUser);
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
 app.use(authRoutes);
-
-// // cookies
-// app.get("/set-cookies", (req, res) => {
-//   // res.setHeader("Set-Cookie", "newUser=true");
-//   res.cookie("newUser", false);
-//   res.cookie("isEmployee", true, {
-//     maxAge: 1000 * 60 * 60 * 24,
-//     httpOnly: true,
-//   });
-//   res.send("you gt the cookies!");
-// });
-
-// app.get("/read-cookies", (req, res) => {
-//   const cookies = req.cookies;
-//   console.log(cookies);
-//   res.json(cookies);
-// });
