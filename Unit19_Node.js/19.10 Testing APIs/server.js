165dd2358f1b1831948a50c4f1651ffdf17357a6
Byeongil Park <bryan1010110@gmail.com>
2025-08-18 23:58:58 -0400

    update

--- Unit19_Node.js/19.10 Testing APIs/server.js
+++ Unit19_Node.js/19.10 Testing APIs/server.js
@@ -2,16 +2,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
 const app = express();
 
 //load configuration from .env file
-require('dotenv-flow').config();
+require("dotenv-flow").config();
 
 //setup Swagger
-const swaggerDocument = YAML.load('./swagger.yaml');
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //import routes and validation
 const authRoutes = require("./routes/auth");
@@ -25,32 +25,39 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Handle CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "auth-token, Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "auth-token, Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-
 //connect to the MongoDB using Mongoose ODM
-mongoose.connect (
-  process.env.DBHOST,  { useUnifiedTopology: true, useNewUrlParser: true }
-).catch(error => console.log("Error connecting to MongoDB: " + error));
+mongoose
+  .connect(process.env.DBHOST, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .catch((error) => console.log("Error connecting to MongoDB: " + error));
 
-mongoose.connection.once('open', () => console.log('Connected succesfully to MongoDB'));
+mongoose.connection.once("open", () =>
+  console.log("Connected succesfully to MongoDB")
+);
 
 //routes definition
 //Welcome route
-app.get("/api/welcome", (req,res) => {
-  res.status(200).send({message: "Welcome to the MEN-REST-API"});
-}); 
+app.get("/api/welcome", (req, res) => {
+  res.status(200).send({ message: "Welcome to the MEN-REST-API" });
+});
 
 // authentication routes to secure the API endpoints
 app.use("/api/user", authRoutes); //authentication routes (register, login)
 app.use("/api/products", productRoutes); //CRUD routes
 
 //start up server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4001;
 app.listen(PORT, function () {
   console.log("Server is running on port:  " + PORT);
 });
