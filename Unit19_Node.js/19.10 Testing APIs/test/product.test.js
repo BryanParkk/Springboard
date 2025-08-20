77a09ef160137eb19c0cfbc0c9b987bedeacecbf
Byeongil Park <bryan1010110@gmail.com>
2025-08-19 22:42:33 -0400

    update

---
+++ Unit19_Node.js/19.10 Testing APIs/test/product.test.js
@@ -0,0 +1,57 @@
+process.env.NODE_ENV = "test";
+
+const Product = require("../models/product");
+const chai = require("chai");
+const expect = chai.expect;
+const should = chai.should();
+const chaiHttp = require("chai-http");
+const server = require("../server");
+
+chai.use(chaiHttp);
+
+before((done) => {
+  Product.deleteMany({}, function (err) {});
+  done();
+});
+
+after((done) => {
+  Product.deleteMany({}, function (err) {});
+  done();
+});
+
+describe("/First Test Collection", () => {
+  it("test default API welcome route", (done) => {
+    chai
+      .request(server)
+      .get("/api/welcome")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        console.log(res.body.message);
+        // res.body.should.be.a("array");
+        const actualVal = res.body.message;
+        expect(actualVal).to.be.equal("Welcome to the MEN-REST-API");
+        done();
+      });
+  });
+
+  it("should verify that we have 0 products in the DB", (done) => {
+    chai
+      .request(server)
+      .get("/api/products")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("array");
+        res.body.length.should.be.eql(0);
+        done();
+      });
+  });
+
+  it("should test two values...", () => {
+    //actual test content in here
+    let expectedVal = 10;
+    let actualVal = 10;
+
+    expect(actualVal).to.be.equal(expectedVal);
+  });
+});
