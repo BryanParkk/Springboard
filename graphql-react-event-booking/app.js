7c7b2cdf048aed9bd9286f0cfc19f68900a2f7f8
Byeongil Park <bryan1010110@gmail.com>
2025-08-21 22:59:35 -0400

    update

--- graphql-react-event-booking/app.js
+++ graphql-react-event-booking/app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req, res, next) => {
+//   res.send("Hello World!");
+// });
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        schema {
+            query: 
+            mutation: 
+        }
+    `),
+    rootValue: {},
+    graphql: true,
+  })
+);
 
 app.listen(3000);
 //
