1708241ef8ce446604e27ee587584ff2ee959b18
Byeongil Park <bryan1010110@gmail.com>
2025-09-01 16:57:43 -0400

    update graphql

--- Unit20_MVC/20.1 GraphQL/graphql-react-event-booking/app.js
+++ Unit20_MVC/20.1 GraphQL/graphql-react-event-booking/app.js
@@ -2,9 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
-const { createEvent } = require("@testing-library/react");
+const mongoose = require("mongoose");
+
+const Event = require("./models/event");
 
 const app = express();
+
+const events = [];
 
 app.use(bodyParser.json());
 
@@ -15,12 +19,27 @@ app.use(
   "/graphql",
   graphqlHTTP({
     schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+
         type RootQuery {
-          events: [String!]!
+          events: [Event!]!
             
         }
         type RootMutation {
-          createEvent(name: String!): String!
+          createEvent(eventInput: EventInput!): Event!
         }
 
         schema {
@@ -29,16 +48,55 @@ app.use(
         }
     `),
     rootValue: {
-      events: (args) => {
-        return ["Romantic Cooking", "Cooking", "Sailing", "All-Night Coding"];
+      events: () => {
+        return Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc, _id: event._doc._id.toString() };
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       },
       createEvent: (args) => {
-        const eventName = args.name;
-        return eventName;
+        const { eventInput } = args;
+        // const event = {
+        //   _id: Math.random().toString(),
+        //   title: eventInput.title,
+        //   description: eventInput.description,
+        //   price: +eventInput.price,
+        //   date: eventInput.date,
+        // };
+        const event = new Event({
+          title: eventInput.title,
+          description: eventInput.description,
+          price: eventInput.price,
+          date: eventInput.date,
+        });
+        return event
+          .save()
+          .then((result) => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch((err) => {
+            console.log(err);
+            throw err;
+          });
       },
     },
     graphiql: true,
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@springboard.mtzjsej.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=SpringBoard`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
