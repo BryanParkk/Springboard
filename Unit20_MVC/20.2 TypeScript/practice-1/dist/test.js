881c44d0b1e2a6d054b0a927cc63a54ea2494812
Byeongil Park <bryan1010110@gmail.com>
2025-09-23 22:32:50 -0400

    update

--- Unit20_MVC/20.2 TypeScript/practice-1/dist/test.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sum(x, y) {
-    console.log(x + y);
-}
-sum(1, 3);
-let color = "red";
-color = "123";
-//# sourceMappingURL=test.js.map
--- Unit20_MVC/20.2 TypeScript/practice-1/src/test.js
+++ Unit20_MVC/20.2 TypeScript/practice-1/src/test.js
@@ -1,36 +1,19 @@
 // function sum(x: number, y: number) {
 //   console.log(x + y);
 // }
-
 // sum(1, 3);
-
-
 // let color = "red";
 // color="123";
 // let score = 99;
 // score = 0;
-
 // let color:string="olive";
 // color="cream";
-
 // let high:any;
-
 // high='google';
-
 // high=2;
-
 // console.log(high);
-
-let firstName = 'Tonya';
-
-firstName = firstName + 1+1;
-
+var firstName = 'Tonya';
+firstName = firstName + 1 + 1;
 console.log(firstName);
-
-
-let names: number[];
-
-names = [2,3,4,5,6]
-
-const stuff: (string | number)[] = [2, 'abc', 3, '3423']
-
+var names;
+names = [2, 3, 4, 5, 6];
