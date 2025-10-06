0586971f662a07078cd94ac843c4905c3a8ac6cc
Byeongil Park <bryan1010110@gmail.com>
2025-10-06 00:53:30 -0400

    update routine padding fix

--- Unit_Proj_Capstone/client/src/features/pages/WorkoutRoutine.jsx
+++ Unit_Proj_Capstone/client/src/features/pages/WorkoutRoutine.jsx
@@ -30,7 +30,7 @@ export default function WorkoutRoutine() {
   }, []);
 
   return (
-    <div className="workoutroutine-container wr-page">
+    <div className="workoutroutine-container">
       <main className="workoutroutine-main">
         <h1 className="headline">Workout Routine</h1>
         <p className="subtitle">Personalized plans to match your goals.</p>
