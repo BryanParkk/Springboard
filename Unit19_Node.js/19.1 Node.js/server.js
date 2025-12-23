73a6fac6a84a27cab8cdf8f1aaf0c769b6a4c20b
Byeongil Park <bryan1010110@gmail.com>
2025-12-22 20:00:00 -0500

    update

--- Unit19_Node.js/19.1 Node.js/server.js
+++ Unit19_Node.js/19.1 Node.js/server.js
@@ -7,10 +7,6 @@ const server = http.createServer((req, res) => {
   // set header content type
   res.setHeader("Content-Type", "text/html");
 
-  //   res.write("<head><link rel=`style sheet` href=`#`></head>");
-  //   res.write("<p>Hello, Bryan</p>");
-  //   res.write("<p>Hello again, Bryan</p>");
-  //   res.end();
   fs.readFile("./views/index.html", (err, data) => {
     if (err) {
       console.log(err);
