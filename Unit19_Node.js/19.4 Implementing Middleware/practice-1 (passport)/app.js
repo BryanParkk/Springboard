f505109a492316b96bd36088b1b1de977de9d04e
Byeongil Park <bryan1010110@gmail.com>
2025-12-23 23:46:37 -0500

    update

--- Unit19_Node.js/19.4 Implementing Middleware/practice-1 (passport)/app.js
+++ Unit19_Node.js/19.4 Implementing Middleware/practice-1 (passport)/app.js
@@ -19,19 +19,10 @@ function errorHandler(err, req, res, next) {
 app.use(middleware1);
 app.use(errorHandler);
 
-// function middleware2(req, res, next) {
-//   console.log("I am a middleware 2");
-//   next();
-// }
-
 function standardExpressCallback(req, res, next) {
   console.log("I am standardExpressCallback function");
   res.send("<h1>Hello World</h1>");
 }
 app.get("/", standardExpressCallback);
-
-// app.get("/", (req, res, next) => {
-//   res.send("<h1>Hello World</h1>");
-// });
 
 app.listen(3000);
