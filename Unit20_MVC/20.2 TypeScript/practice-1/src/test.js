d5d49f83bf55bbfac00189345f191768692d7bab
Byeongil Park <bryan1010110@gmail.com>
2026-02-18 10:33:13 -0500

    update

--- Unit20_MVC/20.2 TypeScript/practice-1/src/test.js
+++ Unit20_MVC/20.2 TypeScript/practice-1/src/test.js
@@ -1,23 +1,20 @@
-// function sum(x: number, y: number) {
-//   console.log(x + y);
-// }
 var admin = {
-    username: "bryan",
-    age: 40,
-    email: "bryan1010110@gmail.com"
+  username: "bryan",
+  age: 40,
+  email: "bryan1010110@gmail.com",
 };
 function printUsername(user) {
-    console.log(user.username);
+  console.log(user.username);
 }
 function printUserAge(user) {
-    console.log(user.age);
+  console.log(user.age);
 }
 function isAdult(user) {
-    return user.age >= 18;
+  return user.age >= 18;
 }
 function makeRandoUser(name) {
-    return {
-        username: name,
-        age: Math.floor(Math.random() * 100),
-    };
+  return {
+    username: name,
+    age: Math.floor(Math.random() * 100),
+  };
 }
